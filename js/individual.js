c91d81fd1767238fa4f7c6a92cd4f8edc5b1aaba
Damian Barrera <>
2021-12-22 18:09:17 -0300

    Mas chicas agregadas

---
+++ js/individual.js
@@ -0,0 +1,67 @@
+const galeria = document.querySelector('.galeria-personal');
+const fotos = document.querySelectorAll('.galeria-personal img')
+const imagen = document.querySelectorAll('.foto');
+const modal = document.querySelector('.modal-foto');
+const imagenInterna = document.querySelector('.img-interna');
+const cerrar = document.querySelector('.close');
+const previous = document.querySelector('.previous');
+const next = document.querySelector('.next');
+let indiceImg = undefined;
+
+ 
+
+
+galeria.addEventListener('click',verImagen);
+previous.addEventListener('click',prevImg);
+next.addEventListener('click',nextImg);
+ 
+cerrar.addEventListener('click',cerrarVentana);
+
+function verImagen(e){
+    if(e.target.classList.contains('img')){
+         imagenInterna.textContent = "";
+         modal.style.display="block";
+         let fotoModal = document.createElement('img');
+         fotoModal.src = e.target.src;
+         imagenInterna.appendChild(fotoModal);
+
+        indiceImg = Array.from(fotos).indexOf(e.target);   
+        comprobarBotonPrev();
+        comprobarBotonNext();
+    }
+ 
+}
+
+function cerrarVentana(e){
+    modal.style.display = "none";
+    imagenInterna.textContent = "";
+}
+function prevImg(){ 
+     imagenInterna.querySelector('img').src = fotos[indiceImg -1].src;
+     indiceImg --;    
+     comprobarBotonPrev();
+     comprobarBotonNext();
+}
+
+ function nextImg(){
+     imagenInterna.querySelector('img').src = fotos[indiceImg +1].src;
+     indiceImg ++;
+     comprobarBotonPrev();
+     comprobarBotonNext();
+ }
+
+  function comprobarBotonPrev(){
+      if(indiceImg === 0) {
+          previous.style.display="none";
+      }else{
+        previous.style.display="block";
+      }
+   }
+
+   function comprobarBotonNext(){
+    if(indiceImg === fotos.length -1){
+        next.style.display="none";
+    }else {
+        next.style.display="block";
+    }
+   }
